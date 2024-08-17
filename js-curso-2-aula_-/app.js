2193f20e8971eaaf1af1ef77025820779b46d549
JaderSC <jader.sirqueira@hotmail.com>
2024-08-17 17:38:01 -0300

    alterei o limite para 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -5,7 +5,7 @@
 //paragrafo.innerHTML = "Escolha um Número entre 1 e 100";
 
 let listadeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial() {
 
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'escolha um Número entre 1 e 10');
+    exibirTextoNaTela('p', 'escolha um Número entre 1 e 100');
 
 }
 
